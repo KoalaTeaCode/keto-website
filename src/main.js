6f9668163bd2df4085c8a2bb5a5b7fdfa2bfd7c6
Keith Holliday <keithrholliday@gmail.com>
2018-03-15 18:04:58 -0500

    Added google analytics

--- src/main.js
+++ src/main.js
@@ -2,13 +2,16 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import Vuex from 'vuex';
+import VueAnalytics from 'vue-analytics';
 
 import App from './App';
 import router from './router';
 import { getStore } from './store';
 
-
 Vue.use(Vuex);
+Vue.use(VueAnalytics, {
+  id: 'UA-111575152-2',
+});
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
